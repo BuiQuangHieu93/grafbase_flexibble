81c32d5b46cd9fb4bf0982b301958bdf0c5a198f
BuiQuangHieu93 <zzzqhieu@gmail.com>
2023-10-02 16:47:19 +0700

    first commit

---
+++ lib/actions.ts
@@ -0,0 +1,41 @@
+import { UserProfile } from "@/common.types";
+import { createUserMutation, getUserQuery } from "@/graphql";
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API || ""
+  : "http://127.0.0.1:4000/graphql";
+
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_KEY || ""
+  : "letmein";
+
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API
+  : "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUser = (email: string) => {
+  return makeGraphQLRequest(getUserQuery, { email });
+};
+
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+  const variables = {
+    input: {
+      name,
+      email,
+      avatarUrl,
+    },
+  };
+  return makeGraphQLRequest(createUserMutation, { variables });
+};
